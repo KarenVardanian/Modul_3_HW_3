2b2365e5beb8874b4db1c5f6d70f8c3e7e2f68e8
KarenVardanian <karen.vardanian@nure.ua>
2021-12-30 20:05:39 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Modul_3_HW_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new A();
+            var b = new B();
+
+            b.ShowHandler = Show;
+            b.ShowHandler(a.Calc(b.Pow, 2, 3).Invoke(2));
+        }
+
+        private static void Show(bool res)
+        {
+            Console.WriteLine(res);
+        }
+    }
+
+    public class A
+    {
+        private double _powResult;
+        private bool _isResult;
+
+        public Predicate<double> Calc(Func<double, double, double> powHandler, double x, double y)
+        {
+            _powResult = powHandler(x, y);
+            Predicate<double> calcDelega=Res;
+            return calcDelega;
+        }
+
+        private bool Res(double x)
+        {
+            return this._powResult % x == 0;
+        }
+    }
+
+    public class B
+    {
+        public Action<bool> ShowHandler;
+
+        public double Pow(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
